d625eb648bcf6d14f92d515d5b0b347a6369fd3c
sehanpratama <keanonoval@gmail.com>
2025-10-06 07:58:21 +0700

    Add files via upload

---
+++ 1-mini-calculator.js
@@ -0,0 +1,47 @@
+const Tambah  = (A, B) => {
+    A = 10
+    B = 100
+    console.log (A + B)
+}
+
+Tambah ()
+
+const Kurang = (A, B) => {
+    A = 10
+    B = 100
+    console.log (A - B)
+}
+
+Kurang ()
+
+const Perkalian = (A, B) => {
+    A = 10
+    B = 100
+    console.log (A * B)
+}
+
+Perkalian ()
+
+const Pembagian = (A, B) => {
+    A = 10
+    B = 100
+    console.log (A / B)
+}
+
+Pembagian ()
+
+const Modulus = (A, B) => {
+    A = 10
+    B = 100
+    console.log (A % B)
+}
+
+Modulus ()
+
+const Perpangkatan = (A, B) => {
+    A = 10
+    B = 100
+    console.log (A ** B)
+}
+
+Perpangkatan ()
